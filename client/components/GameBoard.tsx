f9b427948dc280d34a6fc9eaf4d8a582dee91130
Dylan Guthrie-Wickens <124533567+dylan-guthrie-wickens@users.noreply.github.com>
2023-06-29 11:55:52 +1200

    gameboard started, components made

---
+++ client/components/GameBoard.tsx
@@ -0,0 +1,82 @@
+import OrangeSquare from './OrangeSquare'
+import RedSquare from './RedSquare'
+import PurpleSquare from './PurpleSquare'
+import GreenSquare from './GreenSquare'
+import BlueSquare from './BlueSquare'
+import {
+  orangeData,
+  redData,
+  purpleData,
+  greenData,
+  blueData,
+} from '../lib/lib'
+
+export default function GameBoard() {
+  const size = {
+    x: '3000px',
+    y: '800px',
+  }
+
+  return (
+    <div
+      className="game-board"
+      style={{ width: size.x, height: size.y, backgroundColor: 'lightblue' }}
+    >
+      {purpleData.map((el) => {
+        return (
+          <PurpleSquare
+            key={el.id}
+            x={el.x}
+            y={el.y}
+            rot={el.rot}
+            content={el.content}
+          />
+        )
+      })}
+      {blueData.map((el) => {
+        return (
+          <BlueSquare
+            key={el.id}
+            x={el.x}
+            y={el.y}
+            rot={el.rot}
+            content={el.content}
+          />
+        )
+      })}
+      {redData.map((el) => {
+        return (
+          <RedSquare
+            key={el.id}
+            x={el.x}
+            y={el.y}
+            rot={el.rot}
+            content={el.content}
+          />
+        )
+      })}
+      {orangeData.map((el) => {
+        return (
+          <OrangeSquare
+            key={el.id}
+            x={el.x}
+            y={el.y}
+            rot={el.rot}
+            content={el.content}
+          />
+        )
+      })}
+      {greenData.map((el) => {
+        return (
+          <GreenSquare
+            key={el.id}
+            x={el.x}
+            y={el.y}
+            rot={el.rot}
+            content={el.content}
+          />
+        )
+      })}
+    </div>
+  )
+}
---
+++ client/lib/lib.ts
@@ -0,0 +1,169 @@
+export const orangeData = [
+  {
+    id: 1,
+    x: '180px', //sideways
+    y: '550px', // up down
+    rot: '-5deg',
+    content: 'Cat school',
+  },
+  {
+    id: 2,
+    x: '300px', //sideways
+    y: '650px', // up down
+    rot: '3deg',
+    content: 'Cat school',
+  },
+  {
+    id: 3,
+    x: '400px', //sideways
+    y: '770px', // up down
+    rot: '-5deg',
+    content: 'Cat school',
+  },
+  {
+    id: 4,
+    x: '530px', //sideways
+    y: '700px', // up down
+    rot: '7deg',
+    content: 'Cat school',
+  },
+  {
+    id: 5,
+    x: '670px', //sideways
+    y: '650px', // up down
+    rot: '-5deg',
+    content: 'Cat school',
+  },
+  {
+    id: 6,
+    x: '350px', //sideways
+    y: '360px', // up down
+    rot: '2deg',
+    content: 'Cat school',
+  },
+  {
+    id: 6,
+    x: '750px', //sideways
+    y: '420px', // up down
+    rot: '-5deg',
+    content: 'Cat school',
+  },
+  {
+    id: 7,
+    x: '920px', //sideways
+    y: '470px', // up down
+    rot: '-5deg',
+    content: 'Cat school',
+  },
+  {
+    id: 8,
+    x: '1440px', //sideways
+    y: '650px', // up down
+    rot: '5deg',
+    content: 'Play hide and seek with owner, get 5 treats',
+  },
+]
+
+export const redData = [
+  {
+    id: 1,
+    x: '50px', //sideways
+    y: '450px', // up down
+    rot: '0deg',
+    content: 'Choose school or owner',
+  },
+  {
+    id: 2,
+    x: '1050px', //sideways
+    y: '510px', // up down
+    rot: '-2deg',
+    content: 'Get married?',
+  },
+  {
+    id: 3,
+    x: '1550px', //sideways
+    y: '540px', // up down
+    rot: '3deg',
+    content: 'Buy a house?',
+  },
+]
+
+export const greenData = [
+  {
+    id: 1,
+    x: '550px', //sideways
+    y: '390px', // up down
+    rot: '-5deg',
+    content: 'Play hide and seek with owner, get 5 treats',
+  },
+  {
+    id: 2,
+    x: '1300px', //sideways
+    y: '680px', // up down
+    rot: '-2deg',
+    content: 'Play hide and seek with owner, get 5 treats',
+  },
+  {
+    id: 2,
+    x: '1300px', //sideways
+    y: '370px', // up down
+    rot: '3deg',
+    content: 'Play hide and seek with owner, get 5 treats',
+  },
+]
+
+export const blueData = [
+  {
+    id: 1,
+    x: '1440px', //sideways
+    y: '410px', // up down
+    rot: '-5deg',
+    content: 'Play hide and seek with owner, get 5 treats',
+  },
+]
+
+export const purpleData = [
+  {
+    id: 1,
+    x: '180px', //sideways
+    y: '390px', // up down
+    rot: '5deg',
+    content: 'Adventurous cat',
+  },
+  {
+    id: 2,
+    x: '800px', //sideways
+    y: '580px', // up down
+    rot: '5deg',
+    content: 'Adventurous cat',
+  },
+  {
+    id: 3,
+    x: '1170px', //sideways
+    y: '630px', // up down
+    rot: '5deg',
+    content: 'Partner',
+  },
+
+  {
+    id: 3,
+    x: '1170px', //sideways
+    y: '430px', // up down
+    rot: '-1deg',
+    content: 'Solo',
+  },
+  {
+    id: 5,
+    x: '1670px', //sideways
+    y: '450px', // up down
+    rot: '1deg',
+    content: 'Invest',
+  },
+  {
+    id: 4,
+    x: '1670px', //sideways
+    y: '650px', // up down
+    rot: '-3deg',
+    content: 'Kittens',
+  },
+]
