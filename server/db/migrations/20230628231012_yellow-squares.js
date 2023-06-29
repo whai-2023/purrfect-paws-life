83a872fa4ba8fcaeac2f4fb5747b8becd8ae2efe
Sarah Park <sarahypark83@gmail.com>
2023-06-29 12:33:58 +1200

    update database

---
+++ server/db/migrations/20230628231012_yellow-squares.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('yellow-squares', (table) => {
+    table.increments('id')
+    table.string('input')
+    table.integer('value')
+    table.integer('x')
+    table.integer('y')
+    table.integer('rotation')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('yellow-squares')
+}
