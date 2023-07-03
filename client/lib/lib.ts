ba7304934eedc8de001910a7a22e172cc956acd7
mathias-bridgman <mathias.bridgman9@gmail.com>
2023-07-03 12:06:12 +1200

    have background fitting

--- client/lib/lib.ts
+++ client/lib/lib.ts
@@ -1,7 +1,7 @@
 export const blueData = [
   {
     id: 1,
-    x: '3575px', //sideways
+    x: '3000px', //sideways
     y: '475px', // up down
     rot: '1deg',
     content: 'FINISH',
@@ -11,8 +11,8 @@ export const blueData = [
 export const redData = [
   {
     id: 1,
-    x: '50px', //sideways
-    y: '450px', // up down
+    x: '220px', //sideways
+    y: '400px', // up down
     rot: '0deg',
     content: 'Choose school or owner',
   },
@@ -80,8 +80,8 @@ export const greenData = [
 export const purpleData = [
   {
     id: 1,
-    x: '180px', //sideways
-    y: '390px', // up down
+    x: '305px', //sideways
+    y: '250px', // up down
     rot: '5deg',
     content: 'CHOOSE OWNER',
   },
@@ -120,5 +120,247 @@ export const purpleData = [
     y: '450px', // up down
     rot: '1deg',
     content: 'INVEST',
+  },
+]
+
+export const yellowSquares = [
+  {
+    id: 1,
+    input: 'Play hide and seek with owner,',
+    value: 5,
+    x: 410,
+    y: 465,
+    rot: 1,
+  },
+  {
+    id: 2,
+    input: 'You find a giant fresh piece of ham in the trash',
+    value: 5,
+    x: 540,
+    y: 590,
+    rot: 3,
+  },
+  {
+    id: 3,
+    input: 'You catch the house mouse',
+    value: 5,
+    x: 640,
+    y: 730,
+    rot: -5,
+  },
+  {
+    id: 4,
+    input: 'You win the cat lottery!',
+    value: 20,
+    x: 780,
+    y: 780,
+    rot: 7,
+  },
+  {
+    id: 5,
+    input: 'You do the impossible! You catch the laser pointer!',
+    value: 10,
+    x: 670,
+    y: 650,
+    rot: -5,
+  },
+  {
+    id: 6,
+    input: 'You learn a new trick',
+    value: 5,
+    x: 350,
+    y: 360,
+    rot: 2,
+  },
+  {
+    id: 7,
+    input: 'You explore a new neighbourhood',
+    value: 0,
+    x: 750,
+    y: 420,
+    rot: -5,
+  },
+  {
+    id: 8,
+    input: 'Owner forgets they already feed you. Two dinners!',
+    value: 10,
+    x: 920,
+    y: 470,
+    rot: -5,
+  },
+  {
+    id: 9,
+    input: 'You have a nice afternoon nap in the sun',
+    value: 5,
+    x: 1440,
+    y: 650,
+    rot: 5,
+  },
+  {
+    id: 10,
+    input: 'You wake your owner up at night',
+    value: -5,
+    x: 1440,
+    y: 430,
+    rot: 2,
+  },
+  {
+    id: 11,
+    input: 'Make biscuits / pies',
+    value: 5,
+    x: 1800,
+    y: 700,
+    rot: -3,
+  },
+  {
+    id: 12,
+    input: 'Your owner knits you a cute outfit',
+    value: 5,
+    x: 1800,
+    y: 400,
+    rot: 1,
+  },
+  {
+    id: 13,
+    input: 'You meet the celebrity, grumpy cat',
+    x: 1950,
+    y: 750,
+    rot: -3,
+  },
+  {
+    id: 14,
+    input: 'Snuggle with your owner',
+    value: 5,
+    x: 1950,
+    y: 350,
+    rot: 1,
+  },
+  {
+    id: 15,
+    input: 'Your owner gives you belly scratches',
+    value: 5,
+    x: 2100,
+    y: 750,
+    rot: -3,
+  },
+  {
+    id: 16,
+    input: 'Give the cutest meow your owner has ever heard',
+    value: 5,
+    x: 2100,
+    y: 350,
+    rot: 1,
+  },
+  {
+    id: 17,
+    input: 'Spend the night sleeping in your owners bed',
+    value: 5,
+    x: 2400,
+    y: 650,
+    rot: -3,
+  },
+  {
+    id: 18,
+    input: 'Learn to high-five with your owner',
+    value: 5,
+    x: 2400,
+    y: 450,
+    rot: 4,
+  },
+  {
+    id: 19,
+    input: 'Cat vomit on the carpet',
+    value: -5,
+    x: 2550,
+    y: 550,
+    rot: 1,
+  },
+  {
+    id: 20,
+    input: 'Get sick from drinking bad water',
+    value: 0,
+    x: 2675,
+    y: 485,
+    rot: -3,
+  },
+  {
+    id: 21,
+    input: 'You get into an alley fight',
+    value: -5,
+    x: 2750,
+    y: 365,
+    rot: 3,
+  },
+  {
+    id: 22,
+    input: 'You oversleep and miss breakfast',
+    value: -5,
+    x: 2865,
+    y: 245,
+    rot: -2,
+  },
+  {
+    id: 23,
+    input: 'Debt collector comes to your door!',
+    value: 0,
+    x: 2975,
+    y: 375,
+    rot: 4,
+  },
+  {
+    id: 24,
+    input: "Break your owner's favourite vase",
+    value: -5,
+    x: 2675,
+    y: 650,
+    rot: 3,
+  },
+  {
+    id: 25,
+    input: "Knock the Christmas tree down. You've ruined Christmas!",
+    value: -10,
+    x: 2775,
+    y: 770,
+    rot: -2,
+  },
+  {
+    id: 26,
+    input: 'Cough a furball onto the carpet',
+    value: -5,
+    x: 2900,
+    y: 700,
+    rot: 1,
+  },
+  {
+    id: 27,
+    input: 'Owner catches you trying to steal food off the kitchen counter',
+    value: -5,
+    x: 3025,
+    y: 600,
+    rot: 3,
+  },
+  {
+    id: 28,
+    input: 'You bite your owner',
+    value: -5,
+    x: 3000,
+    y: 375,
+    rot: -2,
+  },
+  {
+    id: 29,
+    input: 'You spill your food bowl all over the floor',
+    value: -5,
+    x: 3000,
+    y: 425,
+    rot: -3,
+  },
+  {
+    id: 30,
+    input: 'You get lost on the way back home',
+    value: 0,
+    x: 3000,
+    y: 550,
+    rot: 2,
   },
 ]
