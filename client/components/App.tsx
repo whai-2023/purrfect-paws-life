9bc37323cc38424c2c0a1b49125a668ce5e610a1
Aiden Hatley <monkeydesu@gmail.com>
2023-07-03 22:51:32 +1200

    Add audio-player

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,19 @@
+import ReactAudioPlayer from 'react-audio-player'
+
 import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <>
       <section>
+        <div className="audioPlayer">
+          <ReactAudioPlayer
+            src={'client/public/audio/derp.mp3'}
+            autoPlay
+            volume={0.4}
+            controls
+            loop
+          />
+        </div>
         <Outlet />
       </section>
     </>
