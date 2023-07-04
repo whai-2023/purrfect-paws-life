218bee5ab41dd750c93d27cd1ba848840eeb7ff8
Aiden Hatley <monkeydesu@gmail.com>
2023-07-05 11:36:34 +1200

    Add popup to footer icons

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,53 +3,11 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import SpinningWheel from './SpinningWheel'
 import useGameStore from '../gameStore'
-import { Popup } from 'semantic-ui-react'
 
 export default function Footer() {
   const { players, activePlayer } = useGameStore()
 
-  // const [isHovering, setIsHovering] = useState(false)
-
-  // const handleMouseOverCatTower = () => {
-  //   setIsHovering(true)
-
-  // }
-
-  // const handleMouseOutCatTower = () => {
-  //   setIsHovering(false)
-  // }
-
-  // const handleMouseOverOwner = () => {
-  //   setIsHovering(true)
-  // }
-
-  // const handleMouseOutOwner = () => {
-  //   setIsHovering(false)
-  // }
-
-  const PopupCatTowers = () => (
-    <Popup
-      content="Plastic $10, Bronze $15"
-      trigger={
-        <img
-          className="catTowerIcon"
-          src="client/public/catImage/cat-tower-icon.png"
-        />
-      }
-    />
-  )
-
-  const PopupOwners = () => (
-    <Popup
-      content="Owner: Aiden $500"
-      trigger={
-        <img
-          className="ownerIcon"
-          src="client/public/catImage/owner-icon.png"
-        />
-      }
-    />
-  )
+  const [isShown, setIsShown] = useState(false)
 
   return (
     <div className="footer">
@@ -57,34 +15,40 @@ export default function Footer() {
       <div className="playerBox">
         <h2>{`Player: ${players[activePlayer - 1].name}`}</h2>
         <h2>{`Treats: ${players[activePlayer - 1].treats}`}</h2>
-        <div
-          className="icons"
-          // onMouseOver={handleMouseOverCatTower}
-          // onMouseOut={handleMouseOutCatTower}
-        >
+        <div className="icons">
           {Array.isArray(players[activePlayer - 1].catTower)
             ? players[activePlayer - 1].catTower.map((el) => {
                 return (
-                  <img
-                    className="catTowerIcon"
-                    src="client/public/catImage/cat-tower-icon.png"
-                  />
+                  <>
+                    <img
+                      onMouseEnter={() => setIsShown(true)}
+                      onMouseLeave={() => setIsShown(false)}
+                      className="catTowerIcon"
+                      src="client/public/catImage/cat-tower-icon.png"
+                    />
+                    {isShown && (
+                      <div className="footerPopup">Plastic $10</div> // HARD CODED FOR NOW
+                    )}
+                  </>
                 )
               })
             : ''}
         </div>
-        <div
-          className="icons"
-          // onMouseOver={handleMouseOverOwner}
-          // onMouseOut={handleMouseOutOwner}
-        >
+        <div className="icons">
           {Array.isArray(players[activePlayer - 1].catTower)
             ? players[activePlayer - 1].owner.map((el) => {
                 return (
-                  <img
-                    className="ownerIcon"
-                    src="client/public/catImage/owner-icon.png"
-                  />
+                  <>
+                    <img
+                      onMouseEnter={() => setIsShown(true)}
+                      onMouseLeave={() => setIsShown(false)}
+                      className="ownerIcon"
+                      src="client/public/catImage/owner-icon.png"
+                    />
+                    {isShown && (
+                      <div className="footerPopup">Aiden $500</div> // HARD CODED FOR NOW
+                    )}
+                  </>
                 )
               })
             : ''}
