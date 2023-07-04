5809babac072a4d36e0680d27e945c08a92412c5
Aiden Hatley <monkeydesu@gmail.com>
2023-07-05 00:13:47 +1200

    Try to add footer hover popups

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,9 +3,53 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import SpinningWheel from './SpinningWheel'
 import useGameStore from '../gameStore'
+import { Popup } from 'semantic-ui-react'
 
 export default function Footer() {
   const { players, activePlayer } = useGameStore()
+
+  // const [isHovering, setIsHovering] = useState(false)
+
+  // const handleMouseOverCatTower = () => {
+  //   setIsHovering(true)
+
+  // }
+
+  // const handleMouseOutCatTower = () => {
+  //   setIsHovering(false)
+  // }
+
+  // const handleMouseOverOwner = () => {
+  //   setIsHovering(true)
+  // }
+
+  // const handleMouseOutOwner = () => {
+  //   setIsHovering(false)
+  // }
+
+  const PopupCatTowers = () => (
+    <Popup
+      content="Plastic $10, Bronze $15"
+      trigger={
+        <img
+          className="catTowerIcon"
+          src="client/public/catImage/cat-tower-icon.png"
+        />
+      }
+    />
+  )
+
+  const PopupOwners = () => (
+    <Popup
+      content="Owner: Aiden $500"
+      trigger={
+        <img
+          className="ownerIcon"
+          src="client/public/catImage/owner-icon.png"
+        />
+      }
+    />
+  )
 
   return (
     <div className="footer">
@@ -13,25 +57,33 @@ export default function Footer() {
       <div className="playerBox">
         <h2>{`Player: ${players[activePlayer - 1].name}`}</h2>
         <h2>{`Treats: ${players[activePlayer - 1].treats}`}</h2>
-        <div className="icons">
+        <div
+          className="icons"
+          // onMouseOver={handleMouseOverCatTower}
+          // onMouseOut={handleMouseOutCatTower}
+        >
           {Array.isArray(players[activePlayer - 1].catTower)
             ? players[activePlayer - 1].catTower.map((el) => {
                 return (
                   <img
-                    className="houseIcon"
-                    src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
+                    className="catTowerIcon"
+                    src="client/public/catImage/cat-tower-icon.png"
                   />
                 )
               })
             : ''}
         </div>
-        <div className="icons">
+        <div
+          className="icons"
+          // onMouseOver={handleMouseOverOwner}
+          // onMouseOut={handleMouseOutOwner}
+        >
           {Array.isArray(players[activePlayer - 1].catTower)
             ? players[activePlayer - 1].owner.map((el) => {
                 return (
                   <img
                     className="ownerIcon"
-                    src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
+                    src="client/public/catImage/owner-icon.png"
                   />
                 )
               })
