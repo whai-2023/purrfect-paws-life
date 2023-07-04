2e0a0da9700725bc4708d331178641a48cc0dcfb
Sarah Park <sarahypark83@gmail.com>
2023-07-04 16:36:51 +1200

    add scoreboard

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,12 @@
-import ReactAudioPlayer from 'react-audio-player'
+// import ReactAudioPlayer from 'react-audio-player'
 
 import { Outlet } from 'react-router-dom'
 import WinningPage from './WinningPage'
 function App() {
   return (
-    <>
-      <section>
-        <WinningPage />
-        <div className="audioPlayer">
+    <main>
+      <WinningPage />
+      {/* <div className="audioPlayer">
           <ReactAudioPlayer
             src={'client/public/audio/derp.mp3'}
             autoPlay
@@ -15,10 +14,9 @@ function App() {
             controls
             loop
           />
-        </div>
-        {/* <Outlet /> */}
-      </section>
-    </>
+        </div> */}
+      {/* <Outlet /> */}
+    </main>
   )
 }
 
