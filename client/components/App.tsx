1f4417bf1137acd8778a9dda040907b4b801a4b7
Aiden Hatley <monkeydesu@gmail.com>
2023-07-04 16:27:24 +1200

    Update character models

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -7,11 +7,12 @@ function App() {
       <section>
         <div className="audioPlayer">
           <ReactAudioPlayer
-            src={'client/public/audio/derp.mp3'}
+            src={'client/public/audio/silly_chicken.mp3'}
             autoPlay
-            volume={0.4}
-            controls
+            volume={0.3}
             loop
+            //TO DO: Remove controls and try to make the audioPlayer play automatically
+            controls
           />
         </div>
         <Outlet />
