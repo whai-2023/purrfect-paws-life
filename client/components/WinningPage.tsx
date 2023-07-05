bab58c1fedc3a510c878ce879d085ea91aefcc5c
Mathias Bridgman <128938519+mathias-bridgman@users.noreply.github.com>
2023-07-05 17:08:12 +1200

    multi song

--- client/components/WinningPage.tsx
+++ client/components/WinningPage.tsx
@@ -2,20 +2,10 @@ import { Link } from 'react-router-dom'
 import Scoreboard from './Scoreboard'
 import Confetti from 'react-confetti'
 import '../styles/winningpage.css'
-import ReactAudioPlayer from 'react-audio-player'
 
 export default function WinningPage() {
   return (
     <div>
-      {/* <div className="audioPlayer">
-        <ReactAudioPlayer
-          src={'/audio/winning-page-music'}
-          autoPlay
-          volume={0.3}
-          loop
-          //TO DO: Remove controls and try to make the audioPlayer play automatically
-          controls
-        /> */}
       <Confetti />
       <div className="confetti">
         <h2>The Winner is Player 1!</h2>
@@ -29,6 +19,5 @@ export default function WinningPage() {
         </div>
       </Link>
     </div>
-    // </div>
   )
 }
